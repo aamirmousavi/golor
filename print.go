58c10d61dcd4631b76b3ce9b220a714281869ac1
Amir Mousavi <38921349+aamirmousavi@users.noreply.github.com>
2022-02-22 12:13:51 +0330

    printer

---
+++ print.go
@@ -0,0 +1,23 @@
+package golor
+
+import "fmt"
+
+type Color string
+
+const (
+	Black   Color = "\033[1;30m%s\033[0m"
+	Red     Color = "\033[1;31m%s\033[0m"
+	Green   Color = "\033[1;32m%s\033[0m"
+	Yellow  Color = "\033[1;33m%s\033[0m"
+	Purple  Color = "\033[1;34m%s\033[0m"
+	Magenta Color = "\033[1;35m%s\033[0m"
+	Teal    Color = "\033[1;36m%s\033[0m"
+	White   Color = "\033[1;37m%s\033[0m"
+)
+
+func Printf(c Color, format string, a ...interface{}) {
+	fmt.Printf(string(c), fmt.Sprintf(format, a...))
+}
+func Sprintf(c Color, format string, a ...interface{}) string {
+	return fmt.Sprintf(string(c), fmt.Sprintf(format, a...))
+}
