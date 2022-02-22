f6486406c7d8cdb00bade50dfc963ac1abc25abb
Amir Mousavi <38921349+aamirmousavi@users.noreply.github.com>
2022-02-22 12:17:26 +0330

    Update print.go

--- print.go
+++ print.go
@@ -5,14 +5,14 @@ import "fmt"
 type Color string
 
 const (
-	Black   Color = "\033[1;30m%s\033[0m"
-	Red     Color = "\033[1;31m%s\033[0m"
-	Green   Color = "\033[1;32m%s\033[0m"
-	Yellow  Color = "\033[1;33m%s\033[0m"
-	Purple  Color = "\033[1;34m%s\033[0m"
-	Magenta Color = "\033[1;35m%s\033[0m"
-	Teal    Color = "\033[1;36m%s\033[0m"
-	White   Color = "\033[1;37m%s\033[0m"
+	BLACK   Color = "\033[1;30m%s\033[0m"
+	RED     Color = "\033[1;31m%s\033[0m"
+	GREEN   Color = "\033[1;32m%s\033[0m"
+	YELLOW  Color = "\033[1;33m%s\033[0m"
+	PURPLE  Color = "\033[1;34m%s\033[0m"
+	MAGENTA Color = "\033[1;35m%s\033[0m"
+	TEAL    Color = "\033[1;36m%s\033[0m"
+	WHITE   Color = "\033[1;37m%s\033[0m"
 )
 
 func Printf(c Color, format string, a ...interface{}) {
@@ -20,4 +20,10 @@ func Printf(c Color, format string, a ...interface{}) {
 }
 func Sprintf(c Color, format string, a ...interface{}) string {
 	return fmt.Sprintf(string(c), fmt.Sprintf(format, a...))
+}
+func Red(format string) string {
+	return fmt.Sprintf(string(RED), format)
+}
+func Black(format string) string {
+	return fmt.Sprintf(string(BLACK), format)
 }
